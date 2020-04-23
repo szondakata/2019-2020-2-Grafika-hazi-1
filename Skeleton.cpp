ed3e865366e69d7ad503f35652abdb146088005b
szondakata <33183035+szondakata@users.noreply.github.com>
2020-04-23 02:11:27 +0200

    Update Skeleton.cpp

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -19,7 +19,7 @@
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
 // Nev    : Szonda Katalin
-// Neptun : FE17LH
+// Neptun : anyad1-0
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
